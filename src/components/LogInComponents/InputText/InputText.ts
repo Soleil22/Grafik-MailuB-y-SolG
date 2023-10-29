219b8c770c063718dfb232baf1c5a90bfea5e45b
Soleil <solangegarciaguamanga@gmail.com>
2023-10-29 12:41:11 -0500

    Input text component almost finish

--- src/components/LogInComponents/InputText/InputText.ts
+++ src/components/LogInComponents/InputText/InputText.ts
@@ -1,1 +1,53 @@
 import EditInputTextCss from "./InputText.css"
+
+export enum InputTextAttributes {
+    "email" = "email",
+    "password"="password",
+    "text"="text",
+}
+
+class InputText extends HTMLElement {
+
+    email?:  string
+    password?: string
+    text?:  string
+
+    static get observedAttributes(){
+        const attrs: Record<InputTextAttributes,null>={
+            email: null,
+            password: null,
+            text: null,
+        }
+        return Object.keys(attrs)
+    }
+
+    attributeChangedCallback(propName:InputTextAttributes,oldValue: string | undefined,newValue: string | undefined){
+        switch(propName){
+            default: 
+            this[propName] = newValue;
+            break;
+        }
+        
+        this.render();
+    }
+
+    constructor(){
+        super()
+        this.attachShadow({mode: "open"})
+    }
+
+    connectedCallback(){
+        this.render()
+    }
+
+    render(){
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML = `
+            <style>${EditInputTextCss}</style>
+            `
+        }
+    }
+}
+
+customElements.define("input-text", InputText)
+export default InputText
