af5903f39d787ebef73f2aca6a522a063accf42e
Soleil <solangegarciaguamanga@gmail.com>
2023-10-29 09:14:58 -0500

    Create Screen LogIn

--- src/screens/LogIn/LogIn.ts
+++ src/screens/LogIn/LogIn.ts
@@ -1,3 +1,25 @@
 import * as components from "../../components/export" 
 import RectangleInformation, {LogInAttributes} from "../../components/LogInComponents/RectangleInformation/RectangleInformation"
 
+class LogInContainer extends HTMLElement {
+
+    constructor(){
+        super()
+        this.attachShadow({mode: "open"})
+    }
+
+    connectedCallback(){
+        this.render()
+    }
+
+    render(){
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML=`
+            `
+            const RectangleContainer = this.ownerDocument.createElement("rectangle-main") as RectangleInformation
+            RectangleContainer.classList.add("Rectangle-information")
+            RectangleContainer.setAttribute(LogInAttributes.grafik, "Grafik.com")
+            RectangleContainer.setAttribute(LogInAttributes.logo, "https://cdn-icons-png.flaticon.com/512/106/106226.png")
+        }
+    }
+}
