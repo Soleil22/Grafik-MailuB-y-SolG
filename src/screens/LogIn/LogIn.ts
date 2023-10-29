c0519bdd4e899aa20303d03083769873b8b6d86f
Soleil <solangegarciaguamanga@gmail.com>
2023-10-29 09:30:10 -0500

    Set attributes Rectangle with information LogIn

--- src/screens/LogIn/LogIn.ts
+++ src/screens/LogIn/LogIn.ts
@@ -16,12 +16,16 @@ class LogInContainer extends HTMLElement {
         if(this.shadowRoot){
             this.shadowRoot.innerHTML=`
             `
+            //Rectangle with information
             const RectangleContainer = this.ownerDocument.createElement("rectangle-main") as RectangleInformation
             RectangleContainer.classList.add("Rectangle-information")
             RectangleContainer.setAttribute(LogInAttributes.grafik, "Grafik.com")
             RectangleContainer.setAttribute(LogInAttributes.logo, "https://cdn-icons-png.flaticon.com/512/106/106226.png")
             RectangleContainer.setAttribute(LogInAttributes.text, "Welcome!")
-            
+            RectangleContainer.setAttribute(LogInAttributes.logo1, "https://cdn-icons-png.flaticon.com/512/59/59439.png")
+            RectangleContainer.setAttribute(LogInAttributes.logo2, "https://static-00.iconduck.com/assets.00/google-logo-icon-2014x2048-hsbglouz.png")
+            RectangleContainer.setAttribute(LogInAttributes.logo3, "https://www.svgrepo.com/show/108614/linkedin.svg")
+            RectangleContainer.setAttribute(LogInAttributes.textsmall, "or use your email")
         }
     }
 }
