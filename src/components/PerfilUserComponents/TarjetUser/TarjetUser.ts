024bec40a865636bc8d5eda18e2b71494082aa76
Soleil <solangegarciaguamanga@gmail.com>
2023-10-30 21:25:12 -0500

    component structure TS

--- src/components/PerfilUserComponents/TarjetUser/TarjetUser.ts
+++ src/components/PerfilUserComponents/TarjetUser/TarjetUser.ts
@@ -1,39 +1,39 @@
 import EditTarjetUserCss from "./TarjetUser.css"
 
-export enum LogInAttributes {
-    "grafik" = "grafik",
-    "logo"="logo",
-    "text"="text",
-    "logo1"="logo1",
-    "logo2"="logo2",
-    "logo3"="logo3",
-    "textsmall"="textsmall"
+export enum TarjetUserAttributes {
+    "perfil" = "perfil",
+    "name"="name",
+    "profession"="profession",
+    "publish"="publish",
+    "bought"="bought",
+    "followers"="followers",
+    "friends"="friends"
 }
 
-class RectangleInformation extends HTMLElement {
+class TarjetUser extends HTMLElement {
 
-    grafik?:  string
-    logo?: string
-    text?:  string
-    logo1?:  string
-    logo2?: string
-    logo3?: string
-    textsmall?: string
+    perfil?:  string
+    name?: string
+    profession?:  string
+    publish?:  string
+    bought?: string
+    followers?: string
+    friends?: string
 
     static get observedAttributes(){
-        const attrs: Record<LogInAttributes,null>={
-            grafik: null,
-            logo: null,
-            text: null,
-            logo1: null,
-            logo2: null,
-            logo3: null,
-            textsmall: null,
+        const attrs: Record<TarjetUserAttributes,null>={
+            perfil: null,
+            name: null,
+            profession: null,
+            publish: null,
+            bought: null,
+            followers: null,
+            friends: null,
         }
         return Object.keys(attrs)
     }
 
-    attributeChangedCallback(propName:LogInAttributes,oldValue: string | undefined,newValue: string | undefined){
+    attributeChangedCallback(propName:TarjetUserAttributes,oldValue: string | undefined,newValue: string | undefined){
         switch(propName){
             default: 
             this[propName] = newValue;
@@ -56,25 +56,10 @@ class RectangleInformation extends HTMLElement {
         if(this.shadowRoot){
             this.shadowRoot.innerHTML = `
             <style>${EditTarjetUserCss}</style>
-            <div class="container">
-            <div class="rectangle-container">
-                    <div class="text-container">
-                    <h3 class="grafik-text">${this.grafik}</h3>
-                    <img src="${this.logo}" alt="" class= "logo-grafik">
-                    <h3 class="text-welcome-join">${this.text}</h3>
-                    </div>
-                    <div class="container-button">
-                    <img src="${this.logo1}" alt="" class= "social-media-buttons1">
-                    <img src="${this.logo2}" alt="" class= "social-media-buttons2">
-                    <img src="${this.logo3}" alt="" class= "social-media-buttons3">
-                    </div>
-                    <p class="small-text">${this.textsmall}</p>
-            </div>
-            </div>
             `
         }
     }
 }
 
-customElements.define("rectangle-information", RectangleInformation)
-export default RectangleInformation
+customElements.define("tarjet-user-information", TarjetUser)
+export default TarjetUser
