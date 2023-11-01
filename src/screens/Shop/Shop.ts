05e38830d397b8063e01a9296c95fac68c5a5a6b
Soleil <solangegarciaguamanga@gmail.com>
2023-11-01 08:34:29 -0500

    add nav left component and CSS

--- src/screens/Shop/Shop.ts
+++ src/screens/Shop/Shop.ts
@@ -5,6 +5,8 @@ import { data } from "../../data/contactData"
 import { dataActivity } from "../../data/lastActivityData"
 import Contacts, {ContactAttributes} from "../../components/dashBoardComponents/contacts/contacts"
 import LastActivity, {ActivityAttributes} from "../../components/dashBoardComponents/lastActivityContacts/lastActivity"
+import EditMainCss from "./main.css"
+import EditShadowCss from "./shadowroot.css"
 
 class Shop extends HTMLElement {
 
@@ -37,12 +39,36 @@ class Shop extends HTMLElement {
         if(this.shadowRoot){
             this.shadowRoot.innerHTML=`
             <style>${EditShopCss}</style>
+            <style>${EditMainCss}</style>
+            <style>${EditShadowCss}</style>
             `
             const NavUpDadContainer = this.ownerDocument.createElement("upper-navigator-icons") as NavUp
             NavUpDadContainer.classList.add("contenedor-papa-navup")
             NavUpDadContainer.setAttribute(NavUpAttribute.appicon, "https://cdn-icons-png.flaticon.com/512/106/106226.png")
             NavUpDadContainer.setAttribute(NavUpAttribute.bell, "https://www.iconpacks.net/icons/2/free-bell-icon-3063-thumb.png" )
             this.shadowRoot.appendChild(NavUpDadContainer)
+
+            //Esto es la NAV
+            const nav = this.ownerDocument.createElement("nav");
+            nav.classList.add("navLeft")
+            
+            const navLeftContainer = this.ownerDocument.createElement("nav-left") as NavLeft
+            navLeftContainer.setAttribute(NavLeftAttributes.username, "Anna!")
+            navLeftContainer.setAttribute(NavLeftAttributes.profileimg, "https://s3-alpha-sig.figma.com/img/022b/9ed5/0cfe89950d617403ba13fe5cd3b92fc2?Expires=1696204800&Signature=B8hSlQTrManrBxpvxZi-x8CG3YTpn1rgpTiNS0Nvu3HQawGz29BvWbz4bANND-Dndc8HcWLoxKPKEh5nofkYemmcJ4c-JH28JpPMcnNkol732tVlh-eE2om1iZJKJjdVtoAg4fA5qIbJBkaOsOFcqXM~UL2dDUVK--RQVjEvywe0oXZNntG6UVz1gSoSb25RgkqWEustu6QVZ722GyEeeC4TQEavJXLLTwl0sx2agxYeqciMtc13myVVyGQevR-eDaEwQOJTstljUb9xcDqlrHNbzUKApF4Dbb~WgUNaIqerBWL7ObZW9z0v4xMG3ej9lwtaNGrN~iJYhHifHPD-Gg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4")
+            nav.appendChild(navLeftContainer)
+            
+            //textos para usar en el index papa
+            const textContact = this.ownerDocument.createElement("h1")
+            textContact.classList.add("textoContacto")
+            textContact.textContent="CONTACTS"
+            
+            const tittleActivity = this.ownerDocument.createElement("h1")
+            tittleActivity.classList.add("tittleActivity")
+            tittleActivity.textContent="LAST ACTIVITY"
+            
+            const textActivity = this.ownerDocument.createElement("p")
+            textActivity.classList.add("textActivity")
+            textActivity.textContent="these are the last people that saw your profile"
 
             
         }
