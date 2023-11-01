0132ed020c28725f5a015b46450a257ff6a6c3e6
Soleil <solangegarciaguamanga@gmail.com>
2023-11-01 08:18:53 -0500

    import components fo the screen perfil post

---
+++ src/screens/PerfilPost/PerfilPost.ts
@@ -0,0 +1,3 @@
+import TarjetUser, {TarjetUserAttributes} from "../../components/PerfilUserComponents/TarjetUser/TarjetUser"
+import NavUp, {NavUpAttribute} from "../../components/PerfilUserComponents/IconsNavUp/IconsNavUp";
+
