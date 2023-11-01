6604987bdeee7c2c2294f19610dd5dedd32db261
Soleil <solangegarciaguamanga@gmail.com>
2023-11-01 00:21:51 -0500

    add component

---
+++ src/components/PerfilUserComponents/IconsNavUp/IconsNavUp.ts
@@ -0,0 +1,56 @@
+import EditNaveUpCss from "./IconsNavUp.css"
+
+export enum NavUpAttribute {
+    "appicon" = "appicon",
+    "bell" = "bell"
+}
+
+class NavUpIcons extends HTMLElement {
+    appicon?: string
+    bell?: string
+
+    static get observedAttributes(){
+        const attrs: Record<NavUpAttribute,null> = {
+            appicon: null,
+            bell: null,
+        }
+        return Object.keys(attrs);
+    }
+
+    attributeChangedCallback(propName:NavUpAttribute,oldValue: string | undefined,newValue: string | undefined){
+        switch(propName){
+            default: 
+            this[propName] = newValue;
+            break;
+        }
+        
+        this.render();
+    }
+
+    constructor(){
+        super();
+        this.attachShadow({mode: "open"});
+    }
+
+    connectedCallback(){
+        this.render()
+    }
+
+    render(){
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML = `
+            <style>${EditNaveUpCss}</style>
+            <div class="nav-bar-container">
+                <div class="icons-container">
+                    <img src="${this.bell}" alt="" class="bell-img">
+                    <button>SIGN OFF</button>
+                    <img src="${this.appicon}" alt="" class="app-img">
+                </div>
+            </div>
+            `
+        }
+    }
+}
+
+customElements.define("upper-navigator", NavUpIcons)
+export default NavUpIcons
