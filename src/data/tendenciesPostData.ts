cd8fbc830935547dc474da8718a7ec4e1d38d82d
Mailu Bermudez <mailub2004@gmail.com>
2023-11-02 01:50:27 -0500

    added and created a temporary data

---
+++ src/data/tendenciesPostData.ts
@@ -0,0 +1,75 @@
+export const tendenciesData = [
+    {
+        username: '@JohnSmith',
+        post: 'https://ejemplo.com/imagen1.jpg'
+    },
+    {
+        username: '@EmilyJohnson',
+        post: 'https://ejemplo.com/imagen2.jpg'
+    },
+    {
+        username: '@MichaelBrown',
+        post: 'https://ejemplo.com/imagen3.jpg'
+    },
+    {
+        username: '@JenniferDavis',
+        post: 'https://ejemplo.com/imagen4.jpg'
+    },
+    {
+        username: '@WilliamWilson',
+        post: 'https://ejemplo.com/imagen5.jpg'
+    },
+    {
+        username: '@OliviaMartinez',
+        post: 'https://ejemplo.com/imagen6.jpg'
+    },
+    {
+        username: '@JamesAnderson',
+        post: 'https://ejemplo.com/imagen7.jpg'
+    },
+    {
+        username: '@LindaHarris',
+        post: 'https://ejemplo.com/imagen8.jpg'
+    },
+    {
+        username: '@DavidLee',
+        post: 'https://ejemplo.com/imagen9.jpg'
+    },
+    {
+        username: '@SophiaClark',
+        post: 'https://ejemplo.com/imagen10.jpg'
+    },
+    {
+        username: '@RobertTaylor',
+        post: 'https://ejemplo.com/imagen11.jpg'
+    },
+    {
+        username: '@EmmaJohnson',
+        post: 'https://ejemplo.com/imagen12.jpg'
+    },
+    {
+        username: '@MatthewMoore',
+        post: 'https://ejemplo.com/imagen13.jpg'
+    },
+    {
+        username: '@AvaWhite',
+        post: 'https://ejemplo.com/imagen14.jpg'
+    },
+    {
+        username: '@WilliamThomas',
+        post: 'https://ejemplo.com/imagen15.jpg'
+    },
+    {
+        username: '@IsabellaWalker',
+        post: 'https://ejemplo.com/imagen16.jpg'
+    },
+    {
+        username: '@JamesDavis',
+        post: 'https://ejemplo.com/imagen17.jpg'
+    },
+    {
+        username: '@SophiaGarcia',
+        post: 'https://ejemplo.com/imagen18.jpg'
+    }
+];
+    
