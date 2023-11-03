fd9b326d11a4aed87486d140956573415e1877f0
Mailu Bermudez <mailub2004@gmail.com>
2023-11-03 02:56:06 -0500

    added post components, data   css to user profile

--- src/data/ProfileUserData.ts
+++ src/data/ProfileUserData.ts
@@ -1,23 +1,29 @@
 export const postProfile = [
     {
-        title: 'my first project',
-        post: 'https://s3-alpha-sig.figma.com/img/a717/261c/c0d0c90dc163ca7a878ac1e4dc5d0e10?Expires=1699833600&Signature=QjFcF0tKAHUcA5z9mm8~fwnuCftVdsI7UrWGE-yfPZoe6uAD8AtkzI8BqJjrX7Kmr7AMEH7lFld7BtrTVxkCWQpdP6DD1ku660lzYZ7CaPY5w9-137p5LuC398x3O7PqoTTgvxueemmjBxcv-QsLOMGeBxYjqX3gthW-1i0AsJzQAbP6tlFNmMGXOHj5fHWpNsM-QlGEEap~kBAhXaA2tOncYRwghMRfK7rN3VIv1mzBzcXwRDzeyeeMm6Y34svuyAZDurA1DLokjmRFf3SP~6Zl259OoIhKEQMzTtaEqlt3hLjZBZjOhXSGaRdWb3YS5Tl1iQadLYnWzbyV1doDLw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+        title: 'Hello. This is My first project',
+        post: "https://designpickle.com/wp-content/uploads/2021/07/072821_10TypOfGraDesNeeToHelYouGetTheConYouNee_BlogGra_Inl5-1024x768.jpg"
     },
     {
-        title: 'painting',
-        post: 'https://s3-alpha-sig.figma.com/img/6624/cc49/50a994d928c7c12868a850d476feb096?Expires=1699833600&Signature=HoljHBCIwCZKRppQKqacJBbdVXhg0XNO55UopqIDZjX-UDpGIXpdfjhaHPjGF-Rc4~HIiQlICp-U1dHyWya7~ovGMN9AzeZhH2p2candsU7FCvbqsVpOu8H6Pvv0Tym5Q5LWvaq~Rwjx1qS3W6ibplhVL6FagqQnrn4ISCt0VsWc4akBA-8MZUl8hf4hSliha~~FEYJM-zewly9j8WFHzs5~HN0t-jQu0lNTEVAqc-ofJotVnhj3YHuhff6-Iah2pTKzb4UG3difghDtb6S~xOVzWNRhO7xD6z~6TeC~nokx3I9tw373SV9-7UpcZDLpEV5E~SKVRtDkKrheYvkD9g__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+        title: 'Loving this palette',
+        post: 'https://images.squarespace-cdn.com/content/v1/5e1106c1d5eea056d6c1b846/1602566863655-38JSQ9UWFAQRMBUYXI2Y/Wet-magazine.jpg?format=750w'
     },
     {
-        title: 'discovering new trends',
-        post: 'https://s3-alpha-sig.figma.com/img/2966/075b/3fb6937ae94b75e8cd5e758871656dfe?Expires=1699833600&Signature=o6s08lGu5dzPvUng3CadO1gYLnsmymmmnq0A4xDt3FtRW8RFyYahSC20AhUP798H1NQQuU4SPRMI6b~waR9Go~RIGtflCUJvSfpZnYUxt8IleP1Bbomwuci~yOUKPVCrU0OukzWEDWGhxzKGGk0dEHkEsfSRONMKMVuj0W5VYmiP2UiKLqrGjb75sDhNxRLbkhbqJ6tbm5g7Cu1qpiphYpe0GXZ0aGN6ddJdCpfu0FAQ-bc~lk~pEVjZrNAQUcyHQ41nxT6PoRbr0q6zhm0oHwGkVd-KRKRhxH3FVyylBQ~Em1Xgz47vQUtgjvRDveJUJutYwSrhWrva~7RFSdNHrQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+        title: 'Currently only painting',
+        post: 'https://nicolesteffen.com/wp-content/uploads/2023/06/isgraphicdesignart-sm.jpg'
     },
     {
-        title: 'hello everyone!',
-        post: 'https://s3-alpha-sig.figma.com/img/f59c/3b7c/8bdead2c10093a3f3dfec02e19eda10b?Expires=1699833600&Signature=fQOF0-ENFlzoKjlsO6Uwi4uUhpVXib~1NMSKjAJPetROg00Y5swX4YozZzJ9IEKAlbhVxySbRckdurg9h1OuS2BKQYxQs8vTt4OX-wZrnthgNRTJjhavVuuBJTS-89x7LeTZ~FAk9vN4wpUxPTlVWxHLHnMH0VJl5ZB9wPDHYkJ804y0fVsbt6B3EKlEQoOw6x-RrFAzhCxeW3W~zSncq0oDb2mh2HdIop1CJ4ME5QZC~q9bq6r1-3FNqmsX9ysGj4NBaWavt4dxZORIbkUzz8jTK34KvJpV69apNgmnfP-Fj6r3-t6sYaIaef7D6pxqVg74FYbz-WLNblWBWhOVXw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
+        title: 'Hello everyone!',
+        post: 'https://i1.wp.com/www.zekagraphic.com/wp-content/uploads/2021/03/Wes-Wilson-Father-Of-Psychedelic-Poster-02.jpg?fit=629%2C1024&ssl=1'
     },
     {
-        title: 'look my last project in Caolofacip',
-        post: 'https://s3-alpha-sig.figma.com/img/83a0/95cc/e101af00a8bc847a6c0f1f2fa0090274?Expires=1699833600&Signature=X5jPZlgV-5SYabIoWOGIenqWmT53n4sB7GfdpCKg2YrfCcfQNvmvkapt6apTIL~2Sxp-~z7NMrdR9nW6tIIZ2OHeZOdqpmIFIYRqgmp0TvsteYV7m8l0Z4tBnHBe4D6dSqGcPGA7L6WPzZVB1kSUL0xPymev6RyL9Onu5bgavubluHa5tg1pUgE-P755GYHntfm24jw0YZCk5MOnFpXqRLzlc4Fu9L0y1e81CDpe2HSMovWaESkEKT0w0BJkJeNM8B~r-CC3IMPhe4Wrsqeavp1EmLouAmibyAfS6PlbM8gU1ot0lctCHr7-iSqI2EuXVbqU~A9Zk5vqwR8-8RIAYw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4'
-    }
+        title: 'Look my last project with Viaduct',
+        post: 'https://blueskygraphics.co.uk/wp-content/uploads/2020/08/Scale-and-visual-hierarchy.jpg'
+    },
+
+    {
+        title: 'Look my last project with Viaduct',
+        post: 'https://blueskygraphics.co.uk/wp-content/uploads/2020/08/Scale-and-visual-hierarchy.jpg'
+    },
+
 
 ]
--- src/screens/PerfilPost/PerfilPost.ts
+++ src/screens/PerfilPost/PerfilPost.ts
@@ -1,10 +1,12 @@
 import EditPerfilPostCss from "./PerfilPost.css"
 import TarjetUser, {TarjetUserAttributes} from "../../components/PerfilUserComponents/TarjetUser/TarjetUser"
 import NavUp, {NavUpAttribute} from "../../components/PerfilUserComponents/IconsNavUp/IconsNavUp";
-import Tendencies,{TendencyAttributes} from "../../components/tendenciesPostsComponents/tendencyPost/tendencyPost";
+import Posts, {PostAttributes} from "../../components/PerfilUserComponents/PostsoftheUser/postUserProfile";
 import { postProfile } from "../../data/ProfileUserData";
 
 class PerfilPostUser extends HTMLElement {
+
+    userp: Posts[]=[]
 
     constructor(){
         super()
@@ -26,6 +28,13 @@ class PerfilPostUser extends HTMLElement {
             NavUpDadContainer.setAttribute(NavUpAttribute.bell, "https://www.iconpacks.net/icons/2/free-bell-icon-3063-thumb.png" )
             this.shadowRoot.appendChild(NavUpDadContainer)
 
+
+            const Title = this.ownerDocument.createElement("h1")
+            Title.classList.add("title-posts")
+            Title.innerText = "Your posts"
+            this.shadowRoot.appendChild(Title)
+
+            
             const TarjetUser = this.ownerDocument.createElement("tarjet-user-information") as TarjetUser
             TarjetUser.classList.add("tarjet-information")
             TarjetUser.setAttribute(TarjetUserAttributes.perfil, "https://s3-alpha-sig.figma.com/img/022b/9ed5/0cfe89950d617403ba13fe5cd3b92fc2?Expires=1699833600&Signature=Y2k0WUtHO5bW3VzWJjyN7Yaj4m4Qs7xp~fw~NUfT5o3W8fbCuNJrWCh~Zxho93yapU8CaacueP4UF~kE4yptnc8N8y8wk5NTgxqV2-HnkTor3juCwCrxbSLMW-1D0JMc5Gxd8Bgix9ChkAgfJDa6uNRodd-0nlNXbD~SuQSd8z5mdaB4iUfG4pKPRXHiau-aX09DO4EcssQ9o8GYNq~zfUWaBfWMdx5JFzZliySJy7DN9158n5W6GCc1qZWsJZ9nTv4vstD7i-okbkfrim~WgAKoWzI1VfDJsrYVRMNhXtkVij8fRA8SCzYHX5Bn54s6L8ZI-nMlO0tE5A9KyBWfbw__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4")
@@ -37,24 +46,22 @@ class PerfilPostUser extends HTMLElement {
             TarjetUser.setAttribute(TarjetUserAttributes.friends, "35")
             this.shadowRoot.appendChild(TarjetUser) 
 
-            postProfile.forEach((postimg)=> {
-                const postDesigner = this.ownerDocument.createElement("post-tendency") as Tendencies
-                    postDesigner.setAttribute(TendencyAttributes.post, postimg.post)
-                    postDesigner.setAttribute(TendencyAttributes.username, postimg.username)
-                    this.posts.push(postDesigner)
+            
+
+            postProfile.forEach((userimg)=> {
+                const postImage = this.ownerDocument.createElement("post-user-component") as Posts
+                    postImage.setAttribute(PostAttributes.imagen, userimg.post)
+                    postImage.setAttribute(PostAttributes.titulo, userimg.title)
+                    this.userp.push(postImage)
                 })
     
-                const PostDadContainer = this.ownerDocument.createElement("section")
-                PostDadContainer.classList.add("contenedor-post-papa")
-                this.posts.forEach((postimg) => {
-                    PostDadContainer.appendChild(postimg)
+                const PostpapaContainer = this.ownerDocument.createElement("section")
+                PostpapaContainer.classList.add("contenedor-post-papa")
+                this.userp.forEach((userimg) => {
+                    PostpapaContainer.appendChild(userimg)
                 })
-                console.log(tendenciesData)
-                this.shadowRoot.appendChild(PostDadContainer)
+                this.shadowRoot.appendChild(PostpapaContainer)
     
-
-
-
 
         }
     }
